7f75aa7545ce8b3426bf45b0a2b78cde91f68ce5
lokhmakov <l.pavel.m@gmail.com>
2022-08-06 07:32:18 +0000

    .

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  return (
+    <>
+      <Head>
+        <title>Reactiflux</title>
+        <meta name="description" content="Reactiflux" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+        Hello World!
+      </main>
+    </>
+  );
+};
+
+export default Home;
